071b9d4fb3567f01356c7373cc5671c2bdb5be6d
Lindsay Carr <lcarr@usgs.gov>
2016-03-28 15:40:05 -0500

    number of records breakout complete

--- R/wqp_overview_stackedbar_records.R
+++ R/wqp_overview_stackedbar_records.R
@@ -2,6 +2,7 @@
 
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 
 data <- read.csv("inst/extdata/wqp_site_chars_all.csv")
 
@@ -21,11 +22,6 @@ data_big <- data %>%
 data_small <- data %>% 
   filter(siteType %in% summary_small$siteType)
 
-# data <- data %>% 
-#   mutate(isLarge = factor(ifelse(siteType %in% data_big$siteType, 
-#                                  "Greater than 25 million records",
-#                                  "Less than 25 million records"))) 
-
 char_cols <- c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c',
                '#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#8dd3c7',
                '#b15928','#bebada','#fb8072','#80b1d3','#fdb462')
@@ -36,7 +32,8 @@ big <- ggplot(data_big, aes(x = siteType,
   geom_bar(stat="identity") + 
   coord_flip() + 
   theme_classic() + 
-  scale_fill_manual(values = char_cols)
+  scale_fill_manual(values = char_cols) + 
+  ggtitle("Greater than 10 million records")
 
 
 small <- ggplot(data_small, aes(x = siteType,
@@ -45,7 +42,21 @@ small <- ggplot(data_small, aes(x = siteType,
   geom_bar(stat="identity") + 
   coord_flip() + 
   theme_classic() + 
-  scale_fill_manual(values = char_cols)
+  scale_fill_manual(values = char_cols) + 
+  ggtitle("Less than 10 million records")
 
-big
-small
+
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+legend <- get_legend(big)
+big <- big + theme(legend.position="none")
+small <- small + theme(legend.position="none")
+
+grid.arrange(big, legend, small, ncol = 2, nrow = 2,
+             layout_matrix = rbind(c(1,2), c(3,2)),
+             widths = c(4,2))
