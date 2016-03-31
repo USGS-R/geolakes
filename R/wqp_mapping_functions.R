303247e42700a660d9b2553cffe3d8b17077cbe5
Jordan S Read <jread@usgs.gov>
2016-03-31 15:27:21 -0500

    adding more vars to make

--- R/wqp_mapping_functions.R
+++ R/wqp_mapping_functions.R
@@ -1,12 +1,4 @@
-library(rgeos)
-library(rgdal)
-library(httr)
-library(dplyr)
-library(RColorBrewer)
-library(dataRetrieval)
 
-missing.data = 'grey90'
-plot.CRS <- "+init=epsg:2163"
 
 get_mutate_HUC8s <- function(config){
   destination = tempfile(pattern = 'huc_shape', fileext='.zip')
@@ -20,56 +12,72 @@ get_mutate_HUC8s <- function(config){
   return(hucs)
 }
 
-# 
-# 
-# lake.sites <- whatWQPsites(characteristicName='Phosphorus') %>% 
-#   select(LongitudeMeasure, LatitudeMeasure) %>% 
-#   rename(lon=LongitudeMeasure, lat=LatitudeMeasure) %>% data.frame
-# 
-# sp2 <- SpatialPoints(lake.sites, proj4string=CRS("+proj=longlat + datum=wgs84")) %>% 
-#   spTransform(CRS(plot.CRS))
-# 
-# ## -- analyze point in polygon /--
-# point.in = gContains(hucs, sp2, byid=TRUE)
-# counts.by.id = colSums(point.in) %>% 
-#   log 
-# 
-# counts.by.id[!is.finite(counts.by.id)] <- 0
-# 
-# 
-# ## -- color markers -- 
-# 
-# key.bins = log(axTicks(1, axp=c(1, max(exp(counts.by.id)), 3), usr=c(1,3), log=TRUE))
-# bins = key.bins
-# pal = colorRampPalette(brewer.pal(9, 'YlGnBu'))(length(bins))
-# key.cols = colorRampPalette(brewer.pal(9, 'YlGnBu'))(length(key.bins))
-# 
-# pal[1] <- missing.data # 0 is grey
-# key.cols[1] <- missing.data  # 0 is grey
-# key.text <- exp(key.bins)
-# key.text[1] <- 0
-# #get closest bin
-# bin = unname(sapply(counts.by.id, function(x) ifelse(is.na(x),NA,which.min(abs(x-bins)))))
-# cols = rep(NA, length(counts.by.id))
-# cols[!is.na(bin)] = pal[bin[!is.na(bin)]]
-# 
-# layout(matrix(data = c(1,1,1,1,1,1,2), ncol=1))
-# par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-# par(mai = c(0,0,0,0), omi = c(0,0,0,0))
-# 
-# xlim <- c(-1534607.9,2050000.1) # specific to the transform we are using
-# ylim <- c(-2072574.6,727758.7)
-# 
-# plot(hucs, add = FALSE, col = cols, border = NA, lwd = 0.5, xlim = xlim, ylim = ylim)
-# 
-# 
-# # secondary plot for color legend
-# plot(c(NA,NA),c(NA,NA), axes=F, ylim=c(0,1),xlim=c(0,1))
-# bin.w = 0.04
-# spc = .02
-# text(.1,.5, 'Number of sites', pos=3, offset=0.1)
-# for(i in 1:length(key.cols)){
-#   x1 = 0.20+(i-1)*(bin.w+spc)
-#   graphics::rect(x1, 0.3, x1+bin.w, 0.8, col=key.cols[i], lwd=NA)
-#   text(x1+bin.w/2, y=0.33, labels=key.text[i], pos=1)
-# }
+get_wqp_data <- function(name, wqp.config, map.config){
+  char = strsplit(name,'[_]')[[1]][1]
+  type = strsplit(name,'[_]')[[1]][2]
+  
+  char.names <- wqp.config[[char]]
+  if (length(char.names[[1]]) > 1){
+    char.names = sapply(char.names[[1]], list)
+    names(char.names) <- rep('characteristicName', length(char.names))
+  }
+
+  args <- append(char.names, wqp.config[[type]])
+  sites <- do.call(whatWQPsites, args) %>% 
+    select(LongitudeMeasure, LatitudeMeasure) %>%
+    rename(lon=LongitudeMeasure, lat=LatitudeMeasure) %>% data.frame %>% 
+    mutate_wqp_site(map.config)
+
+  return(sites)
+}
+
+mutate_wqp_site <- function(sites, config){
+  SpatialPoints(sites, proj4string=CRS("+proj=longlat + datum=wgs84")) %>% 
+    spTransform(CRS(config$plot_CRS))
+}
+
+plot_huc_sites <- function(hucs, sites, map.config, figure.name){
+  point.in = gContains(hucs, sites, byid=TRUE)
+  counts.by.id = colSums(point.in) %>% log
+  
+  counts.by.id[!is.finite(counts.by.id)] <- 0
+
+  ## -- color markers --
+
+  key.bins = log(axTicks(1, axp=c(1, max(exp(counts.by.id)), 3), usr=c(1,3), log=TRUE))
+  bins = key.bins
+  pal = colorRampPalette(brewer.pal(9, 'YlGnBu'))(length(bins))
+  key.cols = colorRampPalette(brewer.pal(9, 'YlGnBu'))(length(key.bins))
+
+  pal[1] <- map.config$missing_data # 0 is grey
+  key.cols[1] <- map.config$missing_data  # 0 is grey
+  key.text <- exp(key.bins)
+  key.text[1] <- 0
+  #get closest bin
+  bin = unname(sapply(counts.by.id, function(x) ifelse(is.na(x),NA,which.min(abs(x-bins)))))
+  cols = rep(NA, length(counts.by.id))
+  cols[!is.na(bin)] = pal[bin[!is.na(bin)]]
+
+  png(filename = figure.name, width = 7, heigh=5, res=300, units = 'in')
+  layout(matrix(data = c(1,1,1,1,1,1,2), ncol=1))
+  par(mai = c(0,0,0,0), omi = c(0,0,0,0))
+  par(mai = c(0,0,0,0), omi = c(0,0,0,0))
+
+  xlim <- c(-1534607.9,2050000.1) # specific to the transform we are using
+  ylim <- c(-2072574.6,727758.7)
+
+  plot(hucs, add = FALSE, col = cols, border = NA, lwd = 0.5, xlim = xlim, ylim = ylim)
+
+
+  # secondary plot for color legend
+  plot(c(NA,NA),c(NA,NA), axes=F, ylim=c(0,1),xlim=c(0,1))
+  bin.w = 0.04
+  spc = .02
+  text(.1,.5, 'Number of sites', pos=3, offset=0.1)
+  for(i in 1:length(key.cols)){
+    x1 = 0.20+(i-1)*(bin.w+spc)
+    graphics::rect(x1, 0.3, x1+bin.w, 0.8, col=key.cols[i], lwd=NA)
+    text(x1+bin.w/2, y=0.33, labels=key.text[i], pos=1)
+  }
+  dev.off()
+}
